a68cdc019891da66f5975107e0f457b4b44287fb
Autumn Valenta <tonightslastsong@gmail.com>
2022-12-03 06:01:46 -0600

    Cover mypy issues
    I can probably control these a little more, but honestly not much.  It's fundamentally upset at use of `map()` on basically everything here

--- 03 Rucksack Reorganization/B__ruthless.py
+++ 03 Rucksack Reorganization/B__ruthless.py
@@ -1,15 +1,15 @@
 import functools
 
 alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-priorities: dict[int, str] = dict(map(reversed, enumerate(alphabet, 1)))
+priorities: dict[int, str] = dict(map(reversed, enumerate(alphabet, 1)))  # type: ignore
 
 with open("data.txt") as f:
     lines = f.readlines()
 
-assert 2641 == sum(map(priorities.get, [
+assert 2641 == sum(map(priorities.get, [  # type: ignore
     functools.reduce(
         set.intersection,
-        map(set, map(str.strip, lines[i:i+3]))
+        map(set, map(str.strip, lines[i:i+3]))  # type: ignore
     ).pop()
     for i in range(0, len(lines), 3)
 ]))
