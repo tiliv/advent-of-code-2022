564ffe5b1391d26e6aa78da4e7c8e4f25230cf59
Autumn Valenta <tonightslastsong@gmail.com>
2022-12-03 16:08:26 -0600

    Drop inner comprehension

--- 01 Calorie Counting/B__ruthless.py
+++ 01 Calorie Counting/B__ruthless.py
@@ -1,5 +1,5 @@
 with open("data.txt") as f:
-    assert 209481 == sum(sorted([
+    assert 209481 == sum(sorted(
         sum(map(int, group.split('\n')))
         for group in f.read().strip().split('\n\n')
-    ])[-3:])
+    )[-3:])
