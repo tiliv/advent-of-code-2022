04a2c3728b68f9eda6354ea44b98a241ec8c0fc1
Autumn Valenta <tonightslastsong@gmail.com>
2022-12-03 17:15:23 -0600

    Oops, tested the wrong file

--- 03 Rucksack Reorganization/B__ruthless.py
+++ 03 Rucksack Reorganization/B__ruthless.py
@@ -6,7 +6,7 @@ with open("data.txt") as f:
     lines = f.readlines()
 
 assert 2641 == sum([
-    1 + alphabet[functools.reduce(set.intersection, sets).pop()]  # type: ignore
+    1 + alphabet.index(functools.reduce(set.intersection, sets).pop())  # type: ignore
     for i in range(0, len(lines), 3)
     if (sets := map(set, map(str.strip, lines[i:i+3])))  # type: ignore
 ])
