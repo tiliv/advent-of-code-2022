8acf404b8a167d7f09c0272e51d67cfc31bc8a03
Autumn Valenta <tonightslastsong@gmail.com>
2022-12-03 17:00:22 -0600

    Switch to an unnecessary walrus

--- 03 Rucksack Reorganization/B__ruthless.py
+++ 03 Rucksack Reorganization/B__ruthless.py
@@ -7,9 +7,7 @@ with open("data.txt") as f:
     lines = f.readlines()
 
 assert 2641 == sum(map(priorities.get, [  # type: ignore
-    functools.reduce(
-        set.intersection,
-        map(set, map(str.strip, lines[i:i+3]))  # type: ignore
-    ).pop()
+    functools.reduce(set.intersection, sets).pop()
     for i in range(0, len(lines), 3)
+    if (sets := map(set, map(str.strip, lines[i:i+3])))  # type: ignore
 ]))
