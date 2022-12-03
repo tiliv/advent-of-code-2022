b828f88c91a3e96a84c053dce6389783c266f6e3
Autumn Valenta <tonightslastsong@gmail.com>
2022-12-03 17:03:23 -0600

    Use indexes instead of equivalent dict

--- 03 Rucksack Reorganization/B__ruthless.py
+++ 03 Rucksack Reorganization/B__ruthless.py
@@ -1,13 +1,12 @@
 import functools
 
 alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-priorities: dict[str, int] = dict(map(reversed, enumerate(alphabet, 1)))  # type: ignore
 
 with open("data.txt") as f:
     lines = f.readlines()
 
-assert 2641 == sum(map(priorities.get, [  # type: ignore
-    functools.reduce(set.intersection, sets).pop()
+assert 2641 == sum([
+    1 + alphabet[functools.reduce(set.intersection, sets).pop()]  # type: ignore
     for i in range(0, len(lines), 3)
     if (sets := map(set, map(str.strip, lines[i:i+3])))  # type: ignore
-]))
+])
