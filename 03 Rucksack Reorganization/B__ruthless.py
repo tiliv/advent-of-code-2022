008ab0d33810a5356b272f88f3f429ee704f8854
Autumn Valenta <tonightslastsong@gmail.com>
2022-12-03 05:31:49 -0600

    Drop batched helper
    It's a shame python doesn't have easy chunking in a single flat reference

--- 03 Rucksack Reorganization/B__ruthless.py
+++ 03 Rucksack Reorganization/B__ruthless.py
@@ -1,16 +1,15 @@
 import functools
-import itertools
 
 alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 priorities: dict[int, str] = dict(map(reversed, enumerate(alphabet, 1)))
 
-def batched(iterable: list, size: int):
-    it = iter(iterable)
-    while (batch := list(itertools.islice(it, size))):
-        yield batch
-
 with open("data.txt") as f:
-    assert 2641 == sum(map(priorities.get, [
-        functools.reduce(set.intersection, map(set, group)).pop()
-        for group in batched(map(str.strip, f.readlines()), size=3)
-    ]))
+    lines = f.readlines()
+
+assert 2641 == sum(map(priorities.get, [
+    functools.reduce(
+        set.intersection,
+        map(set, map(str.strip, lines[i:i+3]))
+    ).pop()
+    for i in range(0, len(lines), 3)
+]))
