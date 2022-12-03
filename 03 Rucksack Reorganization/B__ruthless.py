910701044d3fbf1715b9743e1fd5bc45c1b89faf
Autumn Valenta <tonightslastsong@gmail.com>
2022-12-03 04:55:14 -0600

    Add 03b

---
+++ 03 Rucksack Reorganization/B__ruthless.py
@@ -0,0 +1,19 @@
+import functools
+import itertools
+
+start, alphabet = 1, "abcdefghijklmnopqrstuvwxyz"
+priorities: dict[int, str] = {
+    **dict(map(reversed, enumerate(alphabet, 1))),
+    **dict(map(reversed, enumerate(alphabet.upper(), 27))),
+}
+
+def batched(iterable: list, size: int):
+    it = iter(iterable)
+    while (batch := list(itertools.islice(it, size))):
+        yield batch
+
+with open("data.txt") as f:
+    assert 2641 == sum(map(priorities.get, [
+        functools.reduce(set.intersection, map(set, group)).pop()
+        for group in batched(map(str.strip, f.readlines()), size=3)
+    ]))
